3286bd2cefa2fb74bee3ad32d0956805fc84e1ee
xanderdh <xander.xnet@gmail.com>
2022-05-06 12:54:38 +0300

    add simple backuping

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 nvim_path="~/.config/nvim/"
 
-#copy
-cp -R ./vim/ ~/.config/nvim
+rm -rf ~/.config/nvim/backup
+mkdir ~/.config/nvim/backup
+cp -R ~/.config/nvim/*.vim ~/.config/nvim/backup
 
+cp -R ./vim/* ~/.config/nvim
 print "Done"

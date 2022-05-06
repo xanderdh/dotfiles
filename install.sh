1fbbe21fee1bfaecab45ca377ebcbd4836b5641d
xanderdh <xander.xnet@gmail.com>
2022-05-06 12:32:13 +0300

    refactor install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 
+nvim_path="~/.config/nvim/"
+
 #copy
-cp ./vim/init.vim ~/.config/nvim/init.vim
-cp ./vim/plugings.vim ~/.config/nvim/plugins.vim
-cp ./vim/colors.vim ~/.config/nvim/colors.vim
-cp ./vim/coc.vim ~/.config/nvim/coc.vim
-cp ./vim/mapping.vim ~/.config/nvim/mapping.vim
+cp -R ./vim/ ~/.config/nvim
+
 print "Done"

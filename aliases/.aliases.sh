f0c3c5c0250d7030d565160fde0f4e7c81aa6121
xanderdh <xander.xnet@gmail.com>
2022-11-11 16:48:32 +0200

    update aliases

--- aliases/.aliases.sh
+++ aliases/.aliases.sh
@@ -1,2 +1,9 @@
-alias vim="nvim"
+#/bin/zsh
+
 alias cc="clear"
+
+alias vim="nvim"
+alias python=python3
+alias pip=pip3
+
+alias zsh-test='for i in $(seq 1 5); do /usr/bin/time /bin/zsh -i -c exit; done'

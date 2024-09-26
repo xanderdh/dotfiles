4f76e036c103d14b971bf6d75e6dc771d3c917c5
Denys Volchenkov <dvolchenkov@ebsco.com>
2024-09-26 12:32:26 +0300

    migrage tsserver to ts_ls

--- nvChad/.config/nvim/lua/configs/lspconfig.lua
+++ nvChad/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "jdtls" }
+local servers = { "html", "cssls", "ts_ls", "jdtls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -14,10 +14,3 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}

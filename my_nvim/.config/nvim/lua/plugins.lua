922813f61e79e3251c04870bcad523033b3f2555
den4ik <xander.xnet@gmail.com>
2025-03-30 01:44:11 +0200

    my_nvim: add cmp

--- my_nvim/.config/nvim/lua/plugins.lua
+++ my_nvim/.config/nvim/lua/plugins.lua
@@ -67,4 +67,39 @@ require("lazy").setup({
 			return require("plugin_config.telescope")
 		end,
 	},
+
+	-- load luasnips + cmp related in insert mode only
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+
+			-- autopairing of (){}[] etc
+			{
+				"windwp/nvim-autopairs",
+				opts = {
+					fast_wrap = {},
+					disable_filetype = { "TelescopePrompt", "vim" },
+				},
+				config = function(_, opts)
+					require("nvim-autopairs").setup(opts)
+
+					-- setup cmp for autopairs
+					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+					require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+				end,
+			},
+
+			-- cmp sources plugins
+			{
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-path",
+			},
+		},
+		opts = function()
+			return require("plugin_config.cmp")
+		end,
+	},
 })

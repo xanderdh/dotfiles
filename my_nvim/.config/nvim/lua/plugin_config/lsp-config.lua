9768c4be6c36746232972607d197c2a214261a87
den4ik <xander.xnet@gmail.com>
2025-03-29 15:23:26 +0200

    my_nvim: add python lsp

--- my_nvim/.config/nvim/lua/plugin_config/lsp-config.lua
+++ my_nvim/.config/nvim/lua/plugin_config/lsp-config.lua
@@ -1,7 +1,7 @@
-local lspconfig = require "lspconfig"
-local servers = { "lua_ls", "html", "cssls", "ts_ls", "jdtls", "yamlls" }
+local lspconfig = require("lspconfig")
+local servers = { "lua_ls", "html", "cssls", "ts_ls", "jdtls", "yamlls", "pylsp" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({})
+	lspconfig[lsp].setup({})
 end

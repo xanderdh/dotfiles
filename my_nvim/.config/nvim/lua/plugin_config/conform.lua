dac52eab6912b7ee5d1dae3cda2dc2d8080efbbe
den4ik <xander.xnet@gmail.com>
2025-03-30 16:36:50 +0300

    my_nvim: refactor tabs

--- my_nvim/.config/nvim/lua/plugin_config/conform.lua
+++ my_nvim/.config/nvim/lua/plugin_config/conform.lua
@@ -14,11 +14,11 @@ local options = {
     graphql = { "prettier" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-}
+  -- format_on_save = {
+    --   -- These options will be passed to conform.format()
+    --   timeout_ms = 500,
+    --   lsp_fallback = true,
+    -- },
+  }
 
-require("conform").setup(options)
+  require("conform").setup(options)
--- my_nvim/.config/nvim/lua/plugins.lua
+++ my_nvim/.config/nvim/lua/plugins.lua
@@ -1,126 +1,126 @@
 require("lazy").setup({
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-	"nvim-tree/nvim-web-devicons",
+  "nvim-tree/nvim-web-devicons",
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		config = function()
-			require("plugin_config.nvim-tree")
-		end,
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("plugin_config.nvim-tree")
+    end,
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
-			indent = { enable = true },
-			highlight = {
-				enable = true,
-				use_languagetree = true,
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
+      indent = { enable = true },
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+      },
+    },
+  },
 
-	-- lsp stuff
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("plugin_config.lspconfig").defaults()
-		end,
-	},
+  -- lsp stuff
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("plugin_config.lspconfig").defaults()
+    end,
+  },
 
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua-language-server",
-				"stylua",
-				"html-lsp",
-				"css-lsp",
-				"prettier",
-			},
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+      },
+    },
+  },
 
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
-	},
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+  },
 
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre", -- format on save
-		config = function()
-			require("plugin_config.conform")
-		end,
-	},
+  {
+    "stevearc/conform.nvim",
+    -- event = "BufWritePre", -- format on save
+    config = function()
+      require("plugin_config.conform")
+    end,
+  },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		cmd = "Telescope",
-		opts = function()
-			return require("plugin_config.telescope")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    opts = function()
+      return require("plugin_config.telescope")
+    end,
+  },
 
-	-- git stuff
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "User FilePost",
-		opts = function()
-			return require("plugin_config.gitsigns")
-		end,
-	},
+  -- git stuff
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "User FilePost",
+    opts = function()
+      return require("plugin_config.gitsigns")
+    end,
+  },
 
-	-- load cmp related in insert mode only
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
+  -- load cmp related in insert mode only
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
 
-			-- snippet plugin
-			{
-				"L3MON4D3/LuaSnip",
-				dependencies = "rafamadriz/friendly-snippets",
-				opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-				config = function(_, opts)
-					require("luasnip").config.set_config(opts)
-					require("plugin_config.luasnip")
-				end,
-			},
+      -- snippet plugin
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets",
+        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+        config = function(_, opts)
+          require("luasnip").config.set_config(opts)
+          require("plugin_config.luasnip")
+        end,
+      },
 
-			-- autopairing of (){}[] etc
-			{
-				"windwp/nvim-autopairs",
-				opts = {
-					fast_wrap = {},
-					disable_filetype = { "TelescopePrompt", "vim" },
-				},
-				config = function(_, opts)
-					require("nvim-autopairs").setup(opts)
+      -- autopairing of (){}[] etc
+      {
+        "windwp/nvim-autopairs",
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+        },
+        config = function(_, opts)
+          require("nvim-autopairs").setup(opts)
 
-					-- setup cmp for autopairs
-					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-					require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-				end,
-			},
+          -- setup cmp for autopairs
+          local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+      },
 
-			-- cmp sources plugins
-			{
-				"hrsh7th/cmp-nvim-lua",
-				"hrsh7th/cmp-nvim-lsp",
-				"hrsh7th/cmp-buffer",
-				"hrsh7th/cmp-path",
-			},
-		},
-		opts = function()
-			return require("plugin_config.cmp")
-		end,
-	},
+      -- cmp sources plugins
+      {
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+      },
+    },
+    opts = function()
+      return require("plugin_config.cmp")
+    end,
+  },
 })

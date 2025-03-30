0a0625cdd5b7f5e92efb960849e69e1d1c80ce58
den4ik <xander.xnet@gmail.com>
2025-03-30 17:33:56 +0300

    my_nvim: update styles

--- my_nvim/.config/nvim/init.lua
+++ my_nvim/.config/nvim/init.lua
@@ -17,6 +17,17 @@ require("options")
 require("keymaps")
 require("plugins")
 
--- TODO: refactor?
+-- TODO: refactor below
+
+require("catppuccin").setup({
+  flavour = "mocha",
+  -- transparent_background = true,
+  styles = {
+    comments = { "italic" },
+  },
+})
+
+vim.o.termguicolors = true
+vim.o.background = "dark"
 vim.cmd.colorscheme("catppuccin")
 require("gitsigns").setup()

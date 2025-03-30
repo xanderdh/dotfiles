2a12eba5f761e2b98834b171b08089f5e46e0294
den4ik <xander.xnet@gmail.com>
2025-03-30 16:19:06 +0300

    my_nvim: update indent options

--- my_nvim/.config/nvim/lua/options.lua
+++ my_nvim/.config/nvim/lua/options.lua
@@ -8,11 +8,17 @@ vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 
--- use spaces for tabs and whatnot
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.shiftround = true
+-- Indenting
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+vim.opt.fillchars = { eob = " " }
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.mouse = "a"
 
 vim.cmd([[ set noswapfile ]])
 vim.cmd([[ set termguicolors ]])
@@ -21,4 +27,3 @@ vim.cmd([[ set termguicolors ]])
 vim.wo.number = true
 
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
-

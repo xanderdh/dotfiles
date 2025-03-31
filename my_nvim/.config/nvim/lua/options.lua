2a17798eeaac3795f2bd50922fbc78847451cf31
den4ik <xander.xnet@gmail.com>
2025-03-31 13:29:42 +0300

    my_nvim: update options

--- my_nvim/.config/nvim/lua/options.lua
+++ my_nvim/.config/nvim/lua/options.lua
@@ -27,3 +27,9 @@ vim.cmd([[ set termguicolors ]])
 vim.wo.number = true
 
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has("win32") ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH

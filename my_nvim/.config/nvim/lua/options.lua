cf66aa58a08361145d1f6ada5000531cce32c8c2
Denys Volchenkov <dvolchenkov@ebsco.com>
2025-08-04 16:08:15 +0300

    vim spelling

--- my_nvim/.config/nvim/lua/options.lua
+++ my_nvim/.config/nvim/lua/options.lua
@@ -39,3 +39,7 @@ local is_windows = vim.fn.has("win32") ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
+
+-- spelling
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true

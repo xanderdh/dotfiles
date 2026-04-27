7fa0cf119528af4f4c7ce644fa82bf9a34eeb947
Denys Volchenkov <dvolchenkov@ebsco.com>
2026-04-27 19:32:13 +0300

    fix: nvim depricated method call

--- my_nvim/.config/nvim/lua/plugin_config/lspconfig.lua
+++ my_nvim/.config/nvim/lua/plugin_config/lspconfig.lua
@@ -30,7 +30,7 @@ end
 
 -- disable semanticTokens
 M.on_init = function(client, _)
-  if client.supports_method("textDocument/semanticTokens") then
+  if client:supports_method("textDocument/semanticTokens") then
     client.server_capabilities.semanticTokensProvider = nil
   end
 end
